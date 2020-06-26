9a648c697b9d69508616935503ac26e05830c597
Ryan Gluys <rgluys@gmail.com>
2020-06-26 14:41:31 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'src/app.dart';
+
+//https://medium.com/flutterpub/architect-your-flutter-project-using-bloc-pattern-part-2-d8dd1eca9ba5
+
+void main() {
+  runApp(App());
+}
